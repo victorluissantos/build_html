8fbc16b9c4f02d0709795f2510cb83e27222b414
root <Santos L. Victor>
2020-01-24 21:26:53 -0300

    editor coletando contentHTML

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ bkLib.onDomLoaded(function() {
 bkLib.onDomLoaded(function() {
      new nicEditor({fullPanel : true}).panelInstance('area2');
 }); // convert text area with id area2 to rich text editor with full panel.
+
+function link_whats(numero) {
+	return $('.whats').html('<a href=\"https://wa.me/'+numero+'\" target=\"_blank\">'+numero+'</a>')
+}
+
+function link_mail(email) {
+	return $('.email').html('<a href=\"mailto:'+email+'\" target=\"_self\">'+email+'</a>')
+}
+
+$('div').on('change', function(e) {
+	$('.texto').html(document.getElementsByClassName('nicEdit-main')[0].innerHTML)
+});
+
